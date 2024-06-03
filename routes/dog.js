ff3219dcda086558f8476e42334500a7c57dba46
youRoy0914 <2101100291ph@cyber-u.ac.jp>
2024-06-03 15:01:17 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-const request = require('request');
+var express= require('express');
+var router= express.Router();
+const request= require('request');
 router.get('/', async(req, res) =>{
-	request('https://dog.ceo/dog-api/', function(error, response, body) {
+	request('https://dog.ceo/api/breeds/image/random', function(error, response, body) {
 		if(!error && response.statusCode == 200) {
 			const data = JSON.parse(body);
 			res.json(data);
